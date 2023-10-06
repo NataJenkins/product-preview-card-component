092c4e526a471fc695ec5cdc953cafcf3b6f96de
Natalia Macias <natajenkins@hotmail.com>
2023-10-05 22:38:21 -0600

    product card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import iconCart from "./assets/images/icon-cart.svg";
+import "./styles.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="card">
+                <div className="picture" />
+                <div className="details">
+                    <div className="label">PERFUME</div>
+                    <div className="title">Gabrielle Essence Eau De Parfum</div>
+                    <div className="description">
+                        A floral, solar and voluptuous interpretation composed
+                        by Oliver Pologe, Perfumer-Creator for the House of
+                        CHANEL
+                    </div>
+                    <div className="prices">
+                        <div className="price">$149.99</div>
+                        <div className="discount">$169.99</div>
+                    </div>
+                    <button className="button">
+                        {" "}
+                        <img src={iconCart} className="icon" alt="icon" />
+                        Add to cart
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
